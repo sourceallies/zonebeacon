d145c2eebf58d95d39fa78b6aab248a505f7801d
Jake Klinker <jklinker1@gmail.com>
2016-02-10 16:10:04 -0800

    add test to confirm commands are executed in order they are received

--- app/src/test/java/com/sourceallies/android/zonebeacon/util/CommandExecutorTest.java
+++ app/src/test/java/com/sourceallies/android/zonebeacon/util/CommandExecutorTest.java
@@ -7,13 +7,19 @@ import com.sourceallies.android.zonebeacon.data.Command;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -23,6 +29,12 @@ public class CommandExecutorTest extends ZoneBeaconRobolectricSuite {
     Activity activity;
     @Mock
     List<Command> commands;
+    @Mock
+    CommandExecutor.SocketConnection socketConnection;
+    @Mock
+    PrintWriter printWriter;
+    @Mock
+    InputStream inputStream;
 
     private CommandExecutor commandExecutor;
 
@@ -67,5 +79,35 @@ public class CommandExecutorTest extends ZoneBeaconRobolectricSuite {
         commandExecutor.sendCommand(command);
 
         verify(commandExecutor, times(3)).execute();
+    }
+
+    @Test
+    public void test_executeCommands_multiple() throws Exception {
+        doReturn(socketConnection).when(commandExecutor)
+                .getOrCreateSocketConnection(any(Command.class));
+        doReturn(printWriter).when(commandExecutor).createPrintWriter(socketConnection);
+        doReturn(inputStream).when(socketConnection).getInputStream();
+
+        Command command1 = new Command("test.ip", 100)
+                .setCommand("test 1");
+        Command command2 = new Command("test.ip", 100)
+                .setCommand("test 2");
+        Command command3 = new Command("test.ip", 100)
+                .setCommand("test 3");
+
+        commandExecutor.setCommands(new ArrayList<Command>());
+        commandExecutor.addCommand(command1);
+        commandExecutor.addCommand(command2);
+        commandExecutor.addCommand(command3);
+
+        commandExecutor.execute();
+
+        Thread.sleep(200);
+
+        InOrder inOrder = inOrder(printWriter);
+
+        inOrder.verify(printWriter).print("test 1\r\n");
+        inOrder.verify(printWriter).print("test 2\r\n");
+        inOrder.verify(printWriter).print("test 3\r\n");
     }
 }
