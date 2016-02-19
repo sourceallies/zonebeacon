98b03621447d79b9ef2eed66c0614fde6e56bbdf
Luke Klinker <lklinker1@gmail.com>
2016-02-18 19:37:21 -0600

    add template tests for data source methods

--- app/src/test/java/com/sourceallies/android/zonebeacon/data/SQLiteQueryTest.java
+++ app/src/test/java/com/sourceallies/android/zonebeacon/data/SQLiteQueryTest.java
@@ -4,11 +4,14 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.sourceallies.android.zonebeacon.ZoneBeaconRobolectricSuite;
+import com.sourceallies.android.zonebeacon.data.model.Gateway;
 import com.sourceallies.android.zonebeacon.util.FixtureLoader;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.robolectric.RuntimeEnvironment;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -44,17 +47,42 @@ public class SQLiteQueryTest extends ZoneBeaconRobolectricSuite {
 
     @Test
     public void test_insertNewGateway() {
+        int originalNum = getTableCount("gateway");
+        source.insertNewGateway("Test Gateway", "192.168.1.100", 11000);
+        int newGateways = getTableCount("gateway") - originalNum;
+
+        assertEquals(1, newGateways);
+    }
+
+    @Test
+    public void test_deleteGateway() {
         source.insertNewGateway("Test Gateway", "192.168.1.100", 11000);
 
-        int numGateways = 0;
-        Cursor cursor = source.getDatabase().rawQuery("SELECT count(*) FROM gateway", null);
+        int originalNum = getTableCount("gateway");
+        source.deleteGateway(1);
+        int newGateways = getTableCount("gateway") - originalNum;
+
+        assertEquals(-1, newGateways);
+    }
+
+    @Test
+    public void test_findGateways() {
+        List<Gateway> gateways = source.findGateways();
+        
+        assertEquals(1, gateways.size());
+        assertEquals("Gateway 1", gateways.get(0).getName());
+    }
+
+    private int getTableCount(String table) {
+        Cursor cursor = source.getDatabase().rawQuery("SELECT count(*) FROM " + table, null);
         if (cursor != null && cursor.moveToFirst()) {
-            numGateways = cursor.getInt(0);
+            int numGateways = cursor.getInt(0);
             cursor.close();
+
+            return numGateways;
         }
 
-        assertEquals(1, numGateways);
-
+        throw new RuntimeException("Error finding table count");
     }
 
     private void insertData() throws Exception {
