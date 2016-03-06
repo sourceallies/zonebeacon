a55ff60c69d6f94cd8cfbe72caec187b52f911be
JeffOsborn <jeffrey-osborn@uiowa.edu>
2016-03-06 17:36:16 -0600

    created initial layout

---
+++ app/src/main/java/com/sourceallies/android/zonebeacon/fragment/AddZoneFragment.java
@@ -0,0 +1,105 @@
+package com.sourceallies.android.zonebeacon.fragment;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.klinker.android.link_builder.Link;
+import com.klinker.android.link_builder.LinkBuilder;
+import com.sourceallies.android.zonebeacon.R;
+import com.sourceallies.android.zonebeacon.data.DataSource;
+import com.sourceallies.android.zonebeacon.data.model.Button;
+
+import java.util.List;
+
+import lombok.Getter;
+
+/**
+ *
+ */
+public class AddZoneFragment extends AbstractSetupFragment{
+    /**
+     * Default constructor for the fragment
+     */
+    public AddZoneFragment() { }
+
+    @Getter
+    private TextInputLayout name;
+    @Getter
+    private ListView buttonList;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_add_zone, container, false);
+
+        // setup the UI elements
+        name = (TextInputLayout) root.findViewById(R.id.name);
+        buttonList = (ListView) root.findViewById(R.id.buttonList);
+
+        return root;
+    }
+
+    /**
+     * Check whether or not the text input layout is empty or not. If it is, set an error and
+     * set up a TextWatcher to clear that error.
+     *
+     * @param input TextInputLayout that is either empty or filled
+     * @return true if the layout is empty, false otherwise
+     */
+    public boolean isEmpty(final TextInputLayout input) {
+        if (TextUtils.isEmpty(input.getEditText().getText())) {
+            // display an error message on the edit text
+            input.setError(getString(R.string.fill_field));
+
+            // used to clear the error message on the edit text
+            input.getEditText().addTextChangedListener(new TextWatcher() {
+                @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+                @Override public void afterTextChanged(Editable editable) { }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                    input.setError("");
+                }
+            });
+            return true;
+        } else {
+            // clear the error
+            input.setError("");
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean isComplete() {
+        boolean complete = true;
+
+        if (isEmpty(name))      complete = false;
+
+        return complete;
+    }
+
+
+    @Override
+    public void save() {
+        String name = getText(this.name);
+
+        DataSource source = DataSource.getInstance(getActivity());
+        source.open();
+        //source.insertNewZone(name, );
+        source.close();
+    }
+
+    private String getText(TextInputLayout input) {
+        return input.getEditText().getText().toString();
+    }
+}
