6731e189634e1ad6587c8bdb45f83f9dbe998817
jchuipek <jordan-chuipek@uiowa.edu>
2016-03-22 18:31:49 -0500

    Basic Layout for create command

---
+++ app/src/main/java/com/sourceallies/android/zonebeacon/fragment/CommandSetupFragment.java
@@ -0,0 +1,104 @@
+package com.sourceallies.android.zonebeacon.fragment;
+
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import com.klinker.android.link_builder.Link;
+import com.klinker.android.link_builder.LinkBuilder;
+import com.sourceallies.android.zonebeacon.R;
+import com.sourceallies.android.zonebeacon.data.DataSource;
+import com.sourceallies.android.zonebeacon.data.model.CommandType;
+
+import lombok.Getter;
+
+public class CommandSetupFragment extends AbstractSetupFragment {
+
+    public CommandSetupFragment() { }
+
+    @Getter
+    private TextInputLayout name;
+
+    @Getter
+    private TextInputLayout loadNumber;
+
+    @Getter
+    private Spinner commandType;
+
+    @Getter
+    private Spinner controllerNumber;
+
+    // private String[]
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_create_command, container, false);
+
+        name = (TextInputLayout) root.findViewById(R.id.name);
+        loadNumber = (TextInputLayout)root.findViewById(R.id.load_number);
+        commandType = (Spinner) root.findViewById(R.id.command_type);
+        controllerNumber = (Spinner) root.findViewById(R.id.controller_number);
+
+        return root;
+    }
+
+    public boolean isEmpty(final TextInputLayout input) {
+        if (TextUtils.isEmpty(input.getEditText().getText())) {
+            // display an error message on the edit text
+            input.setError(getString(R.string.fill_field));
+
+            // used to clear the error message on the edit text
+            input.getEditText().addTextChangedListener(new TextWatcher() {
+                @Override public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) { }
+                @Override public void afterTextChanged(Editable editable) { }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                    input.setError("");
+                }
+            });
+            return true;
+        } else {
+            input.setError("");
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean isComplete() {
+        boolean complete = true;
+
+        if (isEmpty(name))             complete = false;
+        if (isEmpty(loadNumber))       complete = false;
+
+        return complete;
+    }
+
+    @Override
+    public void save() {
+        String name = getText(this.name);
+        //int gatewayId = Integer.parseInt(getText(this.gatewayID));
+        int loadNum = Integer.parseInt((getText(this.loadNumber)));
+
+        DataSource source = DataSource.getInstance(getActivity());
+        source.open();
+        source.insertNewCommand(name, gatewayId, loadNumber , command, controllerNum);
+        source.close();
+    }
+
+    private String getText(TextInputLayout input) {
+        return input.getEditText().getText().toString();
+    }
+}
