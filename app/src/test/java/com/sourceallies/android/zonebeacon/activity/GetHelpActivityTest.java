8f28b9892d957e0573f458edb10c2a2193fef2fd
Luke Klinker <lklinker1@gmail.com>
2016-03-27 18:10:35 -0500

    add a test and clean a few things up

--- app/src/test/java/com/sourceallies/android/zonebeacon/activity/GetHelpActivityTest.java
+++ app/src/test/java/com/sourceallies/android/zonebeacon/activity/GetHelpActivityTest.java
@@ -1,9 +1,12 @@
 package com.sourceallies.android.zonebeacon.activity;
 
+import android.content.Intent;
+
 import com.sourceallies.android.zonebeacon.ZoneBeaconRobolectricSuite;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.robolectric.Robolectric;
 
 import static org.junit.Assert.assertNotNull;
@@ -14,11 +17,17 @@ public class GetHelpActivityTest extends ZoneBeaconRobolectricSuite {
 
     @Before
     public void setUp() {
-        activity = Robolectric.setupActivity(GetHelpActivity.class);
+        activity = Mockito.spy(Robolectric.setupActivity(GetHelpActivity.class));
     }
 
     @Test
     public void test_notNull() {
         assertNotNull(activity);
+    }
+
+    @Test
+    public void test_videoLink() {
+        activity.getClickListener(activity).onClick("this YouTube channel");
+        Mockito.verify(activity).startActivity(Mockito.any(Intent.class));
     }
 }
