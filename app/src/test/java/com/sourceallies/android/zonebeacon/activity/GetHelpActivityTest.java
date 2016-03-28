c5c4917360eeacc85d7dd8b31d4a3cccf711e175
Luke Klinker <lklinker1@gmail.com>
2016-03-28 15:45:42 -0500

    add tests

--- app/src/test/java/com/sourceallies/android/zonebeacon/activity/GetHelpActivityTest.java
+++ app/src/test/java/com/sourceallies/android/zonebeacon/activity/GetHelpActivityTest.java
@@ -1,7 +1,9 @@
 package com.sourceallies.android.zonebeacon.activity;
 
 import android.content.Intent;
+import android.view.MenuItem;
 
+import com.sourceallies.android.zonebeacon.R;
 import com.sourceallies.android.zonebeacon.ZoneBeaconRobolectricSuite;
 
 import org.junit.Before;
@@ -10,6 +12,8 @@ import org.mockito.Mockito;
 import org.robolectric.Robolectric;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class GetHelpActivityTest extends ZoneBeaconRobolectricSuite {
 
@@ -23,11 +27,30 @@ public class GetHelpActivityTest extends ZoneBeaconRobolectricSuite {
     @Test
     public void test_notNull() {
         assertNotNull(activity);
+        assertNotNull(activity.getVideoLink());
     }
 
     @Test
     public void test_videoLink() {
         activity.getClickListener(activity).onClick("this YouTube channel");
         Mockito.verify(activity).startActivity(Mockito.any(Intent.class));
+    }
+
+    @Test
+    public void test_option_home() {
+        MenuItem item = Mockito.mock(MenuItem.class);
+
+        when(item.getItemId()).thenReturn(android.R.id.home);
+        activity.onOptionsItemSelected(item);
+        verify(activity).finish();
+    }
+
+    @Test
+    public void test_option_default() {
+        MenuItem item = Mockito.mock(MenuItem.class);
+
+        when(item.getItemId()).thenReturn(R.id.diagnosis);
+        activity.onOptionsItemSelected(item);
+        verify(activity).onOptionsItemSelected(item);
     }
 }
