ed34b26b1d2fe45758f04ca73f4b103fcdf9dcc5
Jake Klinker <jklinker1@gmail.com>
2016-04-07 12:20:27 -0500

    successfully publish and subscribe to nearby

--- app/src/test/java/com/sourceallies/android/zonebeacon/activity/TransferActivityTest.java
+++ app/src/test/java/com/sourceallies/android/zonebeacon/activity/TransferActivityTest.java
@@ -16,10 +16,18 @@
 
 package com.sourceallies.android.zonebeacon.activity;
 
+import android.app.Activity;
+import android.app.PendingIntent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
 import com.sourceallies.android.zonebeacon.ZoneBeaconRobolectricSuite;
 
 import org.junit.Before;
@@ -28,18 +36,35 @@ import org.mockito.Mock;
 import org.robolectric.Robolectric;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
+// NOTE: a lot of these tests are garbage, google play services isn't easy to unit test correctly.
+//       someone should probably come in an improve these at a later date.
 public class TransferActivityTest extends ZoneBeaconRobolectricSuite {
 
     private TransferActivity activity;
 
     @Mock
     private MenuItem menuItem;
+    @Mock
+    private GoogleApiClient client;
+    @Mock
+    private PendingIntent pendingIntent;
 
     @Before
     public void setUp() {
-        activity = Robolectric.setupActivity(TransferActivity.class);
+        activity = getActivity();
+    }
+
+    private TransferActivity getActivity() {
+        TransferActivity activity = spy(Robolectric.buildActivity(TransferActivity.class).create().get());
+        when(activity.createClient()).thenReturn(client);
+        activity.onStart();
+        return activity;
     }
 
     @Test
@@ -50,12 +75,26 @@ public class TransferActivityTest extends ZoneBeaconRobolectricSuite {
     @Test
     public void test_apiClientNull() {
         assertNull(activity.getClient());
+        activity.onClick(null, 0); // click the ok button on the dialog
+        assertNotNull(activity.getClient());
     }
 
     @Test
     public void test_apiClientNotNullOnSecondLaunch() {
-        activity = Robolectric.setupActivity(TransferActivity.class);
+        activity = getActivity();
         assertNotNull(activity.getClient());
+    }
+
+    @Test
+    public void test_messageNotNullOnSecondLaunch() {
+        activity = getActivity();
+        assertNotNull(activity.getMessage());
+    }
+
+    @Test
+    public void test_messageListenerInvokesOnFound() {
+        Message message = new Message("test".getBytes());
+        activity.getMessageListener().onFound(message);
     }
 
     @Test
@@ -84,6 +123,11 @@ public class TransferActivityTest extends ZoneBeaconRobolectricSuite {
     }
 
     @Test
+    public void test_onStop() {
+        activity.onStop();
+    }
+
+    @Test
     public void test_onConnectionSuspended() {
         activity.onConnectionSuspended(0);
     }
@@ -91,6 +135,129 @@ public class TransferActivityTest extends ZoneBeaconRobolectricSuite {
     @Test
     public void test_onConnectionFailed() {
         activity.onConnectionFailed(null);
+    }
+
+    @Test
+    public void test_publish() {
+        try {
+            activity.publish(getRealClient(), new Message("test".getBytes()));
+        } catch (IllegalStateException e) {
+            // GoogleApiClient is not connected yet.
+        }
+    }
+
+    @Test
+    public void test_subscribe() {
+        try {
+            activity.subscribe(getRealClient(), new MessageListener() {
+                @Override
+                public void onFound(Message message) {
+
+                }
+            });
+        } catch (IllegalStateException e) {
+            // GoogleApiClient is not connected yet.
+        }
+    }
+
+    @Test
+    public void test_unpublish() {
+        try {
+            activity.unpublish(getRealClient(), new Message("test".getBytes()));
+        } catch (IllegalStateException e) {
+            // GoogleApiClient is not connected yet.
+        }
+    }
+
+    @Test
+    public void test_unsubscribe() {
+        try {
+            activity.unsubscribe(getRealClient(), new MessageListener() {
+                @Override
+                public void onFound(Message message) {
+
+                }
+            });
+        } catch (IllegalStateException e) {
+            // GoogleApiClient is not connected yet.
+        }
+    }
+
+    @Test
+    public void test_handleUnsuccessful_alreadyResolving() {
+        Status status = new Status(0);
+        activity.setResolvingError(true);
+        activity.handleUnsuccessfulNearbyResult(status);
+    }
+
+    @Test
+    public void test_handleUnsuccessful_hasResolution() {
+        Status status = new Status(0, "test", pendingIntent);
+        activity.setResolvingError(false);
+        activity.handleUnsuccessfulNearbyResult(status);
+    }
+
+    @Test
+    public void test_handleUnsuccessful_networkError() {
+        Status status = new Status(CommonStatusCodes.NETWORK_ERROR);
+        activity.setResolvingError(false);
+        activity.handleUnsuccessfulNearbyResult(status);
+    }
+
+    @Test
+    public void test_handleUnsuccessful_otherError() {
+        Status status = new Status(0);
+        activity.setResolvingError(false);
+        activity.handleUnsuccessfulNearbyResult(status);
+    }
+
+    @Test
+    public void test_onResult_successful() {
+        Status status = new Status(0);
+        activity.onResult(status);
+    }
+
+    @Test
+    public void test_onResult_unsuccessful() {
+        Status status = new Status(1);
+        activity.onResult(status);
+    }
+
+    @Test
+    public void test_onActivityResult_successful() {
+        activity.setResolvingError(true);
+        activity.onActivityResult(1, Activity.RESULT_OK, null);
+        assertFalse(activity.isResolvingError());
+    }
+
+    @Test
+    public void test_onActivityResult_cancelled() {
+        activity.setResolvingError(true);
+        activity.onActivityResult(1, Activity.RESULT_CANCELED, null);
+        assertFalse(activity.isResolvingError());
+        assertTrue(activity.isFinishing());
+    }
+
+    @Test
+    public void test_onActivityResult_other() {
+        activity.setResolvingError(true);
+        activity.onActivityResult(1, 1, null);
+        assertFalse(activity.isResolvingError());
+    }
+
+    @Test
+    public void test_onActivityResult_otherRequestCode() {
+        activity.setResolvingError(true);
+        activity.onActivityResult(2, Activity.RESULT_OK, null);
+        assertTrue(activity.isResolvingError());
+    }
+
+    private GoogleApiClient getRealClient() {
+        return new GoogleApiClient.Builder(activity)
+                .addApi(Nearby.MESSAGES_API)
+                .addConnectionCallbacks(activity)
+                .addOnConnectionFailedListener(activity)
+                .build();
     }
 
 }
