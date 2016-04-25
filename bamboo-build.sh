42be9fc3334b9b4d5862b33dd1c141b2c784e3c3
Kylee Tilley <ktilley@sourceallies.com>
2016-04-25 09:03:35 -0500

    Adding gradle and android sdk install in build script

--- bamboo-build.sh
+++ bamboo-build.sh
@@ -2,4 +2,43 @@
 
 set -e
 
-./gradlew build test
+# GRADLE
+
+GRADLE_VERSION=2.6
+
+curl -LO https://services.gradle.org/distributions/gradle-${GRADLE_VERSION}-all.zip
+
+unzip -f gradle-${GRADLE_VERSION}-all.zip
+
+GRADLE_HOME=gradle-${GRADLE_VERSION}
+PATH=$PATH:$GRADLE_HOME/bin
+
+# ANDROID
+
+# download android sdk
+curl -LO http://dl.google.com/android/android-sdk_r24.2-linux.tgz
+
+tar -xvf android-sdk_r24.2-linux.tgz
+
+# install all sdk packages
+#./android-sdk-linux/tools/android update sdk --no-ui
+
+expect -c '
+set timeout -1;
+spawn android - update sdk --no-ui;
+expect {
+    "Do you accept the license" { exp_send "y\r" ; exp_continue }
+    eof
+}
+'
+
+export PATH=${PATH}:$HOME/sdk/android-sdk-linux/platform-tools:$HOME/sdk/android-sdk-linux/tools:$HOME/sdk/android-sdk-linux/build-tools/22.0.1/
+
+# adb
+sudo apt-get install libc6:i386 libstdc++6:i386
+# aapt
+sudo apt-get install zlib1g:i386
+
+# BUILD
+
+gradlew build test
