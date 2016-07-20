13d624ae5c922274884591b44453afd37e72fef1
Luke Klinker <lklinker1@gmail.com>
2016-07-20 11:12:43 -0500

    move the fetch keystore command to another script

--- build_script.sh
+++ build_script.sh
@@ -1,13 +1,2 @@
-curl -o keystore.jks ${LINK_TO_KEYSTORE}
-
-cat > keystore.properties << EOF
-keystorepassword=${KEY_STORE_PASSWORD}
-keyalias=${KEY_ALIAS}
-keypassword=${KEY_PASSWORD}
-EOF 
-
-cat > api_keys.properties << EOF
-NEARBY=${NEARBY}
-EOF 
-
+./fetch_keystore.sh
 ./gradlew clean assemble testDebugUnitTestCoverage
---
+++ fetch_keystore.sh
@@ -0,0 +1,11 @@
+curl -o keystore.jks ${LINK_TO_KEYSTORE}
+
+cat > keystore.properties << EOF
+keystorepassword=${KEY_STORE_PASSWORD}
+keyalias=${KEY_ALIAS}
+keypassword=${KEY_PASSWORD}
+EOF 
+
+cat > api_keys.properties << EOF
+NEARBY=${NEARBY}
+EOF
