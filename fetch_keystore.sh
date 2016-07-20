93ef1c3fb4c49349dd3d78abf70e9a456c474c81
Luke Klinker <lklinker1@gmail.com>
2016-07-20 14:58:00 -0500

    fetch private keystores from amazon s3

--- fetch_keystore.sh
+++ fetch_keystore.sh
@@ -1,4 +1,16 @@
-curl -o keystore.jks ${LINK_TO_KEYSTORE}
+if [[ ${LINK_TO_KEYSTORE} == *"s3.amazonaws.com"* ]]; then
+	# if the key is stored in amazon s3, then we will assume the bucket is private.
+	contentType="application/octet-stream" 
+	dateValue="`date +'%a, %d %b %Y %H:%M:%S %z'`" 
+	stringToSign="GET\n\n${contentType}\n${dateValue}\n${resource}"
+	signature=`/bin/echo -en "$stringToSign" | openssl sha1 -hmac ${S3_SECRET} -binary | base64` 
+
+	curl -H "Date: ${dateValue}" -H "Content-Type: ${contentType}" -H "Authorization: AWS ${S3_KEY}:${signature}" ${LINK_TO_KEYSTORE} -o "keystore.jks"
+else
+	# implementation for a publicly downloadable keystore (not recommended)
+	curl -o keystore.jks ${LINK_TO_KEYSTORE}
+fi
+
 
 cat > keystore.properties << EOF
 keystorepassword=${KEY_STORE_PASSWORD}
